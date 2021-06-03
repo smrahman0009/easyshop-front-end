9b71bdcd6ca3f4eb09af3a67bec657360bc64fd1
mushfik <smrahman0009@gmail.com>
2021-06-03 15:16:57 +0600

    Shopping cart visible

---
+++ src/layouts/components/products/cart/Cart.jsx
@@ -0,0 +1,10 @@
+import "./Cart.css"
+
+const Cart=()=>{
+    return(
+        <div className="cart-container">
+            Hello
+        </div>
+    )
+}
+export default Cart
--- src/layouts/pages/Home.jsx
+++ src/layouts/pages/Home.jsx
@@ -6,10 +6,13 @@ import Carousel from "../components/carousel/carousel";
 import Container from "../components/container/container";
 import Footer from "../components/footer/Footer";
 import Details from "../components/products/details/Details"
+import Cart from "../components/products/cart/Cart"
+
 const Home=()=>{
     return (
         <>
         <Navbar/>
+        <Cart/>
         <Switch>
             <Route exact path="/">
                 <Carousel/>
