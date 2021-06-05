04348a386de4d7fda9985610fa46d5e0227d677d
mushfik <smrahman0009@gmail.com>
2021-06-05 15:14:38 +0600

    cross button, cart header added

--- src/assets/fontawesome/FontAwesome.js
+++ src/assets/fontawesome/FontAwesome.js
@@ -1,5 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import {faCartPlus,faPhoneAlt,faPhone,faTrashAlt} from '@fortawesome/free-solid-svg-icons'
+import {faCartPlus,faPhoneAlt,faPhone,faTrashAlt,faTimes} from '@fortawesome/free-solid-svg-icons'
 
-library.add(fab,faCartPlus,faPhoneAlt,faPhone,faTrashAlt)
+library.add(fab,faCartPlus,faPhoneAlt,faPhone,faTrashAlt,faTimes)
--- src/layouts/components/products/cart/Cart.jsx
+++ src/layouts/components/products/cart/Cart.jsx
@@ -4,8 +4,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import item_gallery_1 from "../../../../assets/item_gallery_1.png"
 const Cart=()=>{
+    const showCart=()=>{
+        // document.querySelector(".cover-container").classList.toggle("cover-container-show")
+        document.querySelector(".cart-container").classList.toggle("cart-container-show")
+        
+      }
     return(
+        
         <div className="cart-container">
+            <div className="cart-header">
+                <h4>Your Items</h4>
+                <div className="cart-header-icon" onClick={showCart}>
+                    <FontAwesomeIcon icon="times" size="2x"></FontAwesomeIcon>
+                </div>
+            </div>
            <div className="cart-product">
                <img src={item_gallery_1} alt="" />
                <div className="cart-product-info">
